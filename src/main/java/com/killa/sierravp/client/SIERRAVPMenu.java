6290bd2c0b610b8d50452bf2a3f092889f08e275
mcgiovanni10@gmail.com <christian.giovanni.tec@gmail.com>
2024-07-04 16:32:11 -0500

    implementacion de quick select

--- src/main/java/com/killa/sierravp/client/SIERRAVPMenu.java
+++ src/main/java/com/killa/sierravp/client/SIERRAVPMenu.java
@@ -4,7 +4,16 @@
  */
 package com.killa.sierravp.client;
 
+import com.killa.sierravp.domain.Alumno;
+import com.killa.sierravp.domain.Curso;
+import com.killa.sierravp.domain.Usuario;
 import com.killa.sierravp.repository.Universidad;
+import com.killa.sierravp.repository.Universidad.FacultadData;
+import com.killa.sierravp.service.AlumnoService;
+import com.killa.sierravp.service.NotaService;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 
 /**
@@ -12,11 +21,55 @@ import com.killa.sierravp.repository.Universidad;
  * @author HITV
  */
 public class SIERRAVPMenu {
+    
+    private static Scanner scanner = new Scanner(System.in);
+    private static NotaService notaService;
+    private static AlumnoService alumnoService;
 
     public static void main(String[] args) {
         //OJO EN el menu tienes que llamar a la clase Universidad y pasarle una facultad comom parametro
-        Universidad universidad = GenerarFacultades.GenerarFacultadesCompletas("Facultad de Medicina Humana");
-
+        String nombreFacultad = "Facultad de Derecho y Ciencia Política";
+        Universidad universidad = GenerarFacultades.GenerarFacultadesCompletas(nombreFacultad);
+        notaService = new NotaService(universidad);
+        alumnoService = new AlumnoService(universidad);
+        
+        FacultadData fd = universidad.obtenerFacultad(nombreFacultad);
+        
+        //elegir una escuela
+        Map<Integer, Universidad.EscuelaData> escuelas = fd.getEscuelas();
+        System.out.println("Escuelas disponibles:");
+        for (Map.Entry<Integer, Universidad.EscuelaData> entry : escuelas.entrySet()) {
+            Integer key = entry.getKey();
+            Universidad.EscuelaData value = entry.getValue();
+            System.out.println("Id[" + key + "] Escuela: " + value.getEscuela().getNombre());
+        }
+        
+        //de la escuela elegida, selecciona 1 curso
+        System.out.print("Seleccione una escuela (número): ");
+        int idEscuela = scanner.nextInt();
+        
+        Universidad.EscuelaData escuelaElegida = escuelas.get(idEscuela);
+        
+        System.out.println("Cursos disponibles:");
+        for(Curso curso: escuelaElegida.getCursos()) {
+            System.out.println("Id[" + curso.getId() + "] Curso: " + curso);
+        }
+        
+        System.out.print("Seleccione un curso (número): ");
+        int idCurso = scanner.nextInt();
+        
+        List<Alumno> alumnos = escuelaElegida.getAlumnos();
+        //alumnos.stream().forEach(al -> {System.out.println("alumno " + al.getNombre());});
+        
+        System.out.print("Digite la posición que requiere buscar: ");
+        int posicionK = scanner.nextInt();
+        
+        Alumno alumnoK = notaService.obtenerKesimoAlumnoPorNotaFinal(alumnos, idCurso, posicionK);
+        if (alumnoK == null) {
+            System.out.println("Alumno no encontrado");
+        } else {
+            System.out.print("El alumno: " + alumnoK + "obtuvo la posición numero " + posicionK);        
+        }
     }
 
 }
--- src/main/java/com/killa/sierravp/service/NotaService.java
+++ src/main/java/com/killa/sierravp/service/NotaService.java
@@ -1,12 +1,23 @@
 package com.killa.sierravp.service;
 
+import com.killa.sierravp.domain.Alumno;
 import com.killa.sierravp.domain.Nota;
+import com.killa.sierravp.repository.NotaRepository;
 import com.killa.sierravp.repository.Universidad;
+import com.killa.sierravp.util.TipoNota;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import com.killa.sierravp.util.AlumnoWrapper;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class NotaService {
     private Universidad universidad;
+    private NotaRepository notaRepository = new NotaRepository();
 
     public NotaService(Universidad universidad) {
         this.universidad = universidad;
@@ -21,4 +32,87 @@ public class NotaService {
                 .filter(nota -> nota.getAlumno().getCodigo() == codigoAlumno)
                 .collect(Collectors.toList());
     }
+    
+    public List<Nota> obtenerNotasPorCodigoAlumnoCodigoCurso(Alumno alumno, int codigoCurso) {
+        List<Nota> notasCurso = new ArrayList<>();
+        Set<Nota> notas = alumno.getNotas();
+        for(Nota nota: notas) {
+            //System.out.println("alumno tiene nota " + nota.getCalificacion() + "del tipo " + nota.getTipo() + "para curso " + nota.getCurso().getId());
+            if (codigoCurso == nota.getCurso().getId()) {
+                notasCurso.add(nota);
+            }
+            
+        }
+        return notasCurso;
+    }
+
+    public Alumno obtenerKesimoAlumnoPorNotaFinal(List<Alumno> alumnos, int idCurso, int posicionK) {
+        List<AlumnoWrapper> alumnosWrapper = new ArrayList<>();
+        //System.out.println("numero de alumnos total:: " + alumnos.size());
+        
+        int iterationCount = 0;
+        int maxIterations = 100;
+        for (int i = 0; i < maxIterations; i++) {
+            iterationCount++;
+            Alumno alumno = alumnos.get(i);
+            //System.out.println("obteniendo notas para alumno " + alumno.getCorreo() + " y curso id " + idCurso);
+            List<Nota> notasAlumno = obtenerNotasPorCodigoAlumnoCodigoCurso(alumno, idCurso); //notaRepository.obtenerNotasPorAlumnoYCurso(alumno.getCodigo(), idCurso);
+            if (notasAlumno.isEmpty()) {
+                //System.out.println("no se encontraron notas para alumno " + alumno.getCorreo() + " y curso id " + idCurso);
+                continue;
+            }
+
+            Optional<Nota> notaFinal = notasAlumno.stream()
+                .filter(nota -> TipoNota.EF == nota.getTipo())
+                .findFirst();
+            
+            if(notaFinal.isPresent()) {
+                AlumnoWrapper aw = new AlumnoWrapper(alumno, notaFinal.get().getCalificacion());
+                System.out.println("alumno " + aw.getAlumno() + "tiene nota final "+ notaFinal.get().getCalificacion());
+                alumnosWrapper.add(aw);
+                
+            } else {
+                System.out.println("El alumno " + alumno.getNombre() + " no tiene nota final para el curso " + idCurso);
+            }
+
+
+        }
+     
+        if(alumnosWrapper.isEmpty()) {
+            return null;
+        }
+        
+        AlumnoWrapper awEncontrado = quickSelect(alumnosWrapper, 0, alumnosWrapper.size() - 1, posicionK - 1);
+        return awEncontrado.getAlumno();
+    }
+    
+    public static AlumnoWrapper quickSelect(List<AlumnoWrapper> alumnos, int low, int high, int k) {
+        if (low == high) {
+            return alumnos.get(low);
+        }
+
+        int pivotIndex = partition(alumnos, low, high);
+        if (k == pivotIndex) {
+            return alumnos.get(k);
+        } else if (k < pivotIndex) {
+            return quickSelect(alumnos, low, pivotIndex - 1, k);
+        } else {
+            return quickSelect(alumnos, pivotIndex + 1, high, k);
+        }
+    }
+
+    private static int partition(List<AlumnoWrapper> alumnos, int low, int high) {
+        AlumnoWrapper pivot = alumnos.get(high);
+        int i = low;
+
+        for (int j = low; j < high; j++) {
+            if (alumnos.get(j).getFinalScore() > pivot.getFinalScore()) {
+                Collections.swap(alumnos, i, j);
+                i++;
+            }
+        }
+        Collections.swap(alumnos, i, high);
+        return i;
+    }
 }
+
